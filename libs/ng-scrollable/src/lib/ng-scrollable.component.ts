5052112a85d9292e451d86e1222b7f297b0c3cf7
doai <nguyendoai7777@gmail.com>
2025-11-09 21:37:48 +0700

    chore: add linter commit

--- libs/ng-scrollable/src/lib/ng-scrollable.component.ts
+++ libs/ng-scrollable/src/lib/ng-scrollable.component.ts
@@ -1,17 +1,22 @@
-import {AfterViewInit, Component, ElementRef, inject, input, OnDestroy, output, viewChild,} from '@angular/core';
-import type {EventListeners} from 'overlayscrollbars';
-import {OverlayScrollbarDeferOption, OverlayScrollbarDestroyed, OverlayScrollbarEventListeners, OverlayScrollbarInitialized, OverlayScrollbarPartialOptions, OverlayScrollbarScroll, OverlayScrollbarUpdated,} from './ng-scrollable.type';
-import {OverlayScrollbarsDirective} from "./ng-scrollable.directive";
+import { AfterViewInit, Component, ElementRef, inject, input, OnDestroy, output, viewChild } from '@angular/core';
+import type { EventListeners } from 'overlayscrollbars';
+import {
+  OverlayScrollbarDeferOption,
+  OverlayScrollbarDestroyed,
+  OverlayScrollbarEventListeners,
+  OverlayScrollbarInitialized,
+  OverlayScrollbarPartialOptions,
+  OverlayScrollbarScroll,
+  OverlayScrollbarUpdated,
+} from './ng-scrollable.type';
+import { OverlayScrollbarsDirective } from './ng-scrollable.directive';
 
 const mergeEventListeners = (emits: EventListeners, events: EventListeners) =>
   (Object.keys(emits) as (keyof EventListeners)[]).reduce<EventListeners>(
     <N extends keyof EventListeners>(obj: EventListeners, name: N) => {
       const emitListener = emits[name];
       const eventListener = events[name];
-      obj[name] = [
-        emitListener,
-        ...(Array.isArray(eventListener) ? eventListener : [eventListener]).filter(Boolean),
-      ];
+      obj[name] = [emitListener, ...(Array.isArray(eventListener) ? eventListener : [eventListener]).filter(Boolean)];
       return obj;
     },
     {}
@@ -24,14 +29,14 @@ const mergeEventListeners = (emits: EventListeners, events: EventListeners) =>
   host: { 'data-overlayscrollbars-initialize': '' },
   template: `
     <div
-        overlayScrollbars
-        [options]="options()"
-        [events]="mergeEvents(events())"
-        [defer]="defer()"
-        data-overlayscrollbars-contents=""
-        #content
+      overlayScrollbars
+      [options]="options()"
+      [events]="mergeEvents(events())"
+      [defer]="defer()"
+      data-overlayscrollbars-contents=""
+      #content
     >
-      <ng-content/>
+      <ng-content />
     </div>
   `,
 })
@@ -47,15 +52,13 @@ export class OverlayScrollbarsComponent implements OnDestroy, AfterViewInit {
   osDestroyed = output<OverlayScrollbarDestroyed>();
   osScroll = output<OverlayScrollbarScroll>();
 
-
   private readonly contentRef = viewChild<ElementRef<HTMLDivElement>>('content');
 
-  readonly osDirective = viewChild('content', {read: OverlayScrollbarsDirective});
+  readonly osDirective = viewChild('content', { read: OverlayScrollbarsDirective });
 
-  private readonly targetRef = inject<ElementRef<HTMLElement>>(ElementRef)
+  private readonly targetRef = inject<ElementRef<HTMLElement>>(ElementRef);
 
-
-  //region Public APO
+  //region Public APi
   osInstance() {
     return this.osDirective()?.osInstance() ?? null;
   }
@@ -65,7 +68,6 @@ export class OverlayScrollbarsComponent implements OnDestroy, AfterViewInit {
   }
 
   //endregion
-
 
   protected mergeEvents(originalEvents: EventListeners | false | null | undefined) {
     return mergeEventListeners(
@@ -97,6 +99,4 @@ export class OverlayScrollbarsComponent implements OnDestroy, AfterViewInit {
   ngOnDestroy() {
     this.osDirective()?.osInstance()?.destroy();
   }
-
-
 }
